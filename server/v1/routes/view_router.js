7a786ea3c821b835146a66c77daf16c4d73c45f1
Syaiful Dwi Anwar <vlack123@gmail.com>
2024-09-18 21:50:11 +0700

    cso

--- server/v1/routes/view_router.js
+++ server/v1/routes/view_router.js
@@ -1,17 +1,6 @@
 import { Router } from "express";
-// import path from "path";
-// import { fileURLToPath } from 'url';
-
-// const __filename = fileURLToPath(import.meta.url); 
-// const __dirname = path.dirname(__filename);
 
 const viewRouter = new Router();
-
-// viewRouter.get('/', async (_req, res) => {
-//     res.sendFile('index.html', {
-//         root: path.join(__dirname, '../public')
-//     });
-// });
 
 viewRouter.get('/', async (_req, res) => {
     res.render('index.pug', { title: 'CSO', pengumuman: true });
