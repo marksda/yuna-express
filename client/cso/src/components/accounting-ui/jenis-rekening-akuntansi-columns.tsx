d8ec1bf4b86a6af019ab303f5a9c5f668eab0d24
marksda <vlack123@gmail.com>
2024-12-03 13:46:58 +0700

    cso

--- client/cso/src/components/accounting-ui/jenis-rekening-akuntansi-columns.tsx
+++ client/cso/src/components/accounting-ui/jenis-rekening-akuntansi-columns.tsx
@@ -1,5 +1,8 @@
 import { ColumnDef } from "@tanstack/react-table"
 import { IJenisRekeningAkuntansi } from "@/features/entities/accounting/jenis-rekening-akuntansi";
+import { MoreHorizontal } from "lucide-react"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
 
 export const JenisRekeningAkuntansiColumns: ColumnDef<IJenisRekeningAkuntansi>[] = [
     {
@@ -10,4 +13,32 @@ export const JenisRekeningAkuntansiColumns: ColumnDef<IJenisRekeningAkuntansi>[]
         accessorKey: "keterangan",
         header: "Keterangan",
     },
+    {
+        id: "actions",
+        cell: ({row}) => {
+            const jnsRA = row.original
+
+            return (
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="ghost"className="h-8 w-8 p-0">
+                            <span className="sr-only">Open menu</span>
+                            <MoreHorizontal className="h-4 w-4" />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                        <DropdownMenuItem
+                            onClick={() => navigator.clipboard.writeText(jnsRA._id)}
+                        >
+                        Copy
+                        </DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>Edit</DropdownMenuItem>
+                        <DropdownMenuItem>View details</DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            )
+        }
+    }
 ]
