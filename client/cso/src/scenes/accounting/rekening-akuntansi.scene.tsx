a46aef365a9854615a0272333f07fe51f6c920dc
marksda <vlack123@gmail.com>
2024-12-14 08:24:56 +0700

    cso

--- client/cso/src/scenes/accounting/rekening-akuntansi.scene.tsx
+++ client/cso/src/scenes/accounting/rekening-akuntansi.scene.tsx
@@ -7,7 +7,9 @@ import _ from "lodash"
 import { Button } from "@/components/ui/button";
 import { Plus } from "lucide-react"
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
-import { SidebarTrigger } from "@/components/ui/sidebar";
+import { SidebarInset, SidebarTrigger } from "@/components/ui/sidebar";
+import { Drawer, DrawerContent, DrawerDescription, DrawerHeader, DrawerTitle, DrawerTrigger } from "@/components/ui/drawer";
+import { Separator } from "@/components/ui/separator";
 
 export const RekeningAkuntansiScene: FC = () => {
   const [filter, setFilter] = useState<IQueryParamFilters>({
@@ -37,28 +39,46 @@ export const RekeningAkuntansiScene: FC = () => {
   const {data} = useGetDaftarRekeningAkuntansiQuery(filter)
 
   return (
-    <>      
-      <div className="flex px-2 justify-between border-b">
-        <div className="flex items-center">
+    <SidebarInset>      
+      <header className="flex h-16 shrink-0 justify-between border-b px-4">
+        <div className="flex items-center gap-2">
           <SidebarTrigger />
+          <Separator orientation="vertical" className="mr-2 h-4" />
           <h3 className="font-semibold">Rekening akuntansi</h3>
-        </div>        
-        <Dialog modal={false}>
-          <DialogTrigger asChild>
-            <Button size="sm" className="my-1">
-              <Plus />New
-            </Button>  
-          </DialogTrigger>
-          <DialogContent className="sm:max-w-[425px]">
-            <DialogHeader>
-              <DialogTitle>Edit profile</DialogTitle>
-              <DialogDescription>
-                Make changes to your profile here. Click save when you're done.
-              </DialogDescription>
-            </DialogHeader>
-          </DialogContent>
-        </Dialog>        
-      </div>         
+        </div>
+        <div className="flex items-center gap-2">
+          <Drawer modal={true} direction="right">
+            <DrawerTrigger asChild>
+              <Button size="sm" className="my-1">
+                <Plus />Drawer New
+              </Button>
+            </DrawerTrigger>
+            <DrawerContent>
+              <div className="mx-auto w-full max-w-sm">
+                <DrawerHeader>
+                  <DrawerTitle>Move Goal</DrawerTitle>
+                  <DrawerDescription>Set your daily activity goal.</DrawerDescription>
+                </DrawerHeader>
+              </div>
+            </DrawerContent>
+          </Drawer>
+          <Dialog modal={false}>
+            <DialogTrigger asChild>
+              <Button size="sm" className="my-1">
+                <Plus />New
+              </Button>  
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+              <DialogHeader>
+                <DialogTitle>Edit profile</DialogTitle>
+                <DialogDescription>
+                  Make changes to your profile here. Click save when you're done.
+                </DialogDescription>
+              </DialogHeader>
+            </DialogContent>
+          </Dialog>        
+        </div>
+      </header>
       <DataTable 
         columns={RekeningAkuntansiColumns} 
         data={data == undefined? []:data} 
@@ -69,6 +89,6 @@ export const RekeningAkuntansiScene: FC = () => {
         isPagination={true}
         title="Data kode rekening"
       />
-    </>
+    </SidebarInset>
   )
 }
